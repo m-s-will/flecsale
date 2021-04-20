4d0ccf0ceb0d43224c0565c2281f8a24757a3dca
Michael Will <57011129+m-s-will@users.noreply.github.com>
2021-04-20 16:12:19 +0200

    Update start_simulation.sh

--- start_simulation.sh
+++ start_simulation.sh
@@ -1,2 +1,1 @@
-#give the container id as a second argument to this script
 mpiexec --allow-run-as-root -n 2 /home/docker/flecsale_build/bin/hydro_3d -m /home/docker/flecsale//apps/catalyst_adaptor/catalyst_scripts/hydro3d_live.py
